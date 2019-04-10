8940f279790b4d0bfa498a79f24e0266e7328ea3
huitian <tanhuitian@gmail.com>
2019-04-10 23:41:31 +0800

    add one more test to addNumbers

--- js/exercises/addNumbers/addNumbers.test.js
+++ js/exercises/addNumbers/addNumbers.test.js
@@ -1,7 +1,10 @@
 const addNumbers = require("./addNumbers");
 
 describe("addNumbers", () => {
-  it("adds two numbers", () => {
+  it("adds 2 to 3 and returns 5", () => {
     expect(addNumbers(2, 3)).toBe(5);
+  });
+  it("adds 5 to 3 and returns 8", () => {
+    expect(addNumbers(5, 3)).toBe(8);
   });
 });
